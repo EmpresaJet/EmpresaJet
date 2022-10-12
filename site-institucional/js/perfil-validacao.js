7190890e525ac0b3f3dc994e3e7d2cad2007171b
gabs-mvb <gabriel.boas@sptech.school>
2022-10-12 12:34:44 -0300

    Validações da tela de cadastro das empresas e retirada do REXGEX das senhas, fiz um jeito mais facil de colocar a senha, no qual todos entendam

---
+++ site-institucional/js/perfil-validacao.js
@@ -0,0 +1,167 @@
+/* Validando os campos da empresa */
+function validarEmpresa() {
+  var empresa = inputEmpresa.value;
+  /* Verifica se o nome da empresa tem mais de 3 caractéres */
+  if (empresa.length < 3) {
+    inputEmpresa.classList.add("red");
+    inputEmpresa.classList.remove("green");
+  } else {
+    inputEmpresa.classList.remove("red");
+    inputEmpresa.classList.add("green");
+  }
+}
+//Validando CNPJ da empresa com os 14 caracteres
+function validarCNPJ() {
+  var CNPJ = inputCNPJ.value;
+  /* Verifica se o usuario tem mais de 6 caractéres */
+  if (CNPJ.length != 14) {
+    inputCNPJ.classList.add("red");
+    inputCNPJ.classList.remove("green");
+  } else {
+    inputCNPJ.classList.remove("red");
+    inputCNPJ.classList.add("green");
+  }
+}
+
+//Validando o Logradouro da empresa
+function validarLogradouro() {
+  var logradouro = inputLogradouro.value;
+  /* Verifica se o logradouro tem mais de 3 caractéres */
+  if (logradouro.length < 3) {
+    inputLogradouro.classList.add("red");
+    inputLogradouro.classList.remove("green");
+  } else {
+    inputLogradouro.classList.remove("red");
+    inputLogradouro.classList.add("green");
+  }
+}
+
+//Validando a cidade da empresa
+function validarCidade() {
+  var cidade = inputCidade.value;
+  /* Verifica se a cidade tem mais de 8 caractéres */
+  if (cidade.length < 8) {
+    inputCidade.classList.add("red");
+    inputCidade.classList.remove("green");
+  } else {
+    inputCidade.classList.remove("red");
+    inputCidade.classList.add("green");
+  }
+}
+
+// Validando o CEP da empresa
+function validarCEP() {
+  var CEP = inputCEP.value;
+  /* Verifica se a cidade tem mais de 8 caractéres */
+  if (CEP.length != 8) {
+    inputCEP.classList.add("red");
+    inputCEP.classList.remove("green");
+  } else {
+    inputCEP.classList.remove("red");
+    inputCEP.classList.add("green");
+  }
+}
+
+//Validando o Bairro da empresa
+function validarBairro() {
+  var bairro = inputBairro.value;
+  //Verifica se o bairro da empresa tem mais que 6 caracteres
+  if (bairro.length < 6) {
+    inputCEP.classList.add("red");
+    inputCEP.classList.remove("green");
+  } else {
+    inputCEP.classList.remove("red");
+    inputCEP.classList.add("green");
+  }
+}
+
+//Validando o Complemento da Empresa
+function validarComplemento() {
+    var complemento = inputBairro.value;
+  //Verifica se o complemento da empresa tem mais que 4 caracteres
+  if (complemento.length < 4) {
+    inputComplemento.classList.add("red");
+    inputComplemento.classList.remove("green");
+  } else {
+    inputComplemento.classList.remove("red");
+    inputComplemento.classList.add("green");
+  }
+} 
+/* Validando os campos do cadastro do funcionario */
+//Validando Nome de usuario
+function validarUsuario() {
+  var usuario = inputUsuario.value;
+  /* Verifica se o usuario tem mais de 6 caractéres */
+  if (usuario.length < 6) {
+    inputUsuario.classList.add("red");
+    inputUsuario.classList.remove("green");
+  } else {
+    inputUsuario.classList.remove("red");
+    inputUsuario.classList.add("green");
+  }
+}
+//Validando Senha do Usuario
+function validarSenha() {
+  var senha = inputSenha.value;
+
+  /* Verifica se a senha está com as requisições acima */
+  if (
+    senha.indexOf(senha.toUpperCase()) == -1 &&
+    senha.indexOf(senha.toLowerCase()) == -1 &&
+    senha.length >= 8
+  ) {
+    inputSenha.classList.remove("red");
+    inputSenha.classList.add("green");
+  } else {
+    inputSenha.classList.add("red");
+    inputSenha.classList.remove("green");
+  }
+}
+//Validando Nome Do usuario
+function validarNome() {
+  var nome = inputNome.value;
+  /* Devolve a quantidade de nomes em numeros Ex: Sherlock Homes -> vai retornar 2 */
+  const nomeCompleto = nome.split(" ");
+  /* Verifica se a quantidade de nomes é menor que 1, pois ninguém tem um nome completo de um nome apenas */
+  if (nomeCompleto.length <= 1) {
+    inputNome.classList.add("red");
+    inputNome.classList.remove("green");
+  } else {
+    inputNome.classList.remove("red");
+    inputNome.classList.add("green");
+  }
+  /* Faz a substituição das primeiras letras dos nomes caso o usuário coloque a primeira leta do nome minuscula */
+  for (let i = 0; i < nomeCompleto.length; i++) {
+    nomeCompleto[i] =
+      nomeCompleto[i][0].toUpperCase() + nomeCompleto[i].substr(1);
+  }
+  /* Junta os nomes novamente */
+  nomeCompleto.join(" ");
+}
+//Validando Email do Usuario
+function validarEmail() {
+  var email = inputEmail.value;
+  /* Qualquer tipo de texto:
+    Seguida por um caractere @ (que é obrigatório em e-mails);
+    E então temos a presença de um ponto, que também é obrigatório;
+    E por fim mais um texto, validando tanto emails .com quanto .com.br, e outros que tenham terminologias diferentes */
+  if (email.indexOf("@") == -1 || email.indexOf(".com") == -1) {
+    inputEmail.classList.add("red");
+    inputEmail.classList.remove("green");
+  } else {
+    inputEmail.classList.remove("red");
+    inputEmail.classList.add("green");
+  }
+}
+//Validando Contato do Usuario
+function validarContato() {
+  var contato = inputContato.value;
+  if (contato.length > 11) {
+    // Valida números telefones celulares para contato
+    inputContato.classList.add("red");
+    inputContato.classList.remove("green");
+  } else {
+    inputContato.classList.remove("red");
+    inputContato.classList.add("green");
+  }
+}
